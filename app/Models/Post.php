c0c6110c99e31b59e9abaed0efc65370a989c918
DangSauQuaKhu <tklmissno1@gmail.com>
2023-01-12 19:57:43 +0700

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $primaryKey = "id";
+
+    protected $fillable = ["title","description","status","user_id"];
+  
+    public $timestamps = true;
+}
